1ecbea8b988af627bd6cfb425368264d05653bf2
Amir Abu Shareb <yields@icloud.com>
2013-10-26 09:11:58 +0300

    wtf

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var slug = require('slug-component');
  * Usage:
  *
  *      mySchema.plugin(slug('title'));
- *      
+ *
  * Options:
- * 
+ *
  *      - `.replace` characters to replace defaulted to `[^a-zA-Z]`
  *      - `.separator` separator to use, defaulted to `-`
  *      - `required` whether a slug is required, defaults to `true`
@@ -25,21 +25,21 @@ var slug = require('slug-component');
 
 module.exports = function(prop, opts){
   return (function slugize(schema){
-		var title;
+    var title;
     schema.add({ slug: String });
     schema.pre('save', function(next){
-			var self = this;
-			
-			if (prop && Array.isArray(prop)) {
-				var titles = [];
-				prop.forEach(function(el){
-					titles.push(self[el]);
-				});				
-				title = titles.join(' ');
-			} else {
-				title = this[prop || 'title'];
-			}
-			
+      var self = this;
+
+      if (prop && Array.isArray(prop)) {
+        var titles = [];
+        prop.forEach(function(el){
+          titles.push(self[el]);
+        });
+        title = titles.join(' ');
+      } else {
+        title = this[prop || 'title'];
+      }
+
       var require = (opts && !opts.required) ? false : true;
       if (require && !title) return next(new Error(prop + ' is required to create a slug'));
       if (title) self.slug = slug(title, opts);
